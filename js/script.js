8dab790bb7990cc2f5b249571253b8a769f29c00
albertoferri <falberto99@gmail.com>
2024-01-25 13:21:57 +0100

    aggiunto colore

--- js/script.js
+++ js/script.js
@@ -6,4 +6,9 @@ document.getElementById('fullname').innerText = `Ciao, il tuo nome è ${fullname
 // inserisco cognome
 const surname = prompt("Qual'è il tuo cognome?");
 
-document.getElementById('surname').innerText = `Ciao, il tuo cognome è ${surname}!`;
+document.getElementById('surname').innerText = `Mentre, il tuo cognome è ${surname}!`;
+
+// chiedo il colore preferito
+const color = prompt("Qual'è il tuo colore preferito?");
+
+document.getElementById('color').innerText = `Infine, il tuo colore preferito è il ${color}!`;
